4138e68fc42472e063f079bf841610bf64b674a7
danakongur <plantdaddyuwu@gmail.com>
2025-04-10 16:10:56 +0000

    Fixed plugin file

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-export default {
-  meta: {
-    type: "problem",
-    docs: {
-      description: "Don't allow the moment library",
-    },
-    fixable: "code",
-    schema: [],
-  },
-  create(context) {
-    return {
-      ImportDeclaration(node) {
-        if (node.source.value == "moment") {
-          context.report({
-            node,
-            message: "Do not use the library 'moment'",
-          });
-        }
-      },
-    };
-  },
+module.exports = {
+  rules: {
+	'no-moment': {
+	  meta: {
+		type: "problem",
+		docs: {
+		  description: "Don't allow the moment library",
+		},
+		fixable: "code",
+		schema: [],
+	  },
+	  create(context) {
+		return {
+		  ImportDeclaration(node) {
+			if (node.source.value == "moment") {
+			  context.report({
+				node,
+				message: "Do not use the library 'moment'",
+			  });
+			}
+		  },
+		};
+	  },
+	};
+  };
 };
