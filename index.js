6cffc3c258cebbfa9b4f377c8f872ab7226c1f51
danakongur <plantdaddyuwu@gmail.com>
2025-04-10 15:41:19 +0000

    Added ESlint rule

---
+++ index.js
@@ -0,0 +1,22 @@
+export default {
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Don't allow the moment library",
+    },
+    fixable: "code",
+    schema: [],
+  },
+  create(context) {
+    return {
+      ImportDeclaration(node) {
+        if (node.source.value == "moment") {
+          context.report({
+            node,
+            message: "Do not use the library 'moment'",
+          });
+        }
+      },
+    };
+  },
+};
