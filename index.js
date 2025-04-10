7b06325b31aa86316f9112323875a2c1f9ff0ace
danakongur <plantdaddyuwu@gmail.com>
2025-04-10 16:20:06 +0000

    Fixed error

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 module.exports = {
   rules: {
-	'no-moment': {
-	  meta: {
-		type: "problem",
-		docs: {
-		  description: "Don't allow the moment library",
-		},
-		fixable: "code",
-		schema: [],
-	  },
-	  create(context) {
-		return {
-		  ImportDeclaration(node) {
-			if (node.source.value == "moment") {
-			  context.report({
-				node,
-				message: "Do not use the library 'moment'",
-			  });
-			}
-		  },
-		};
-	  },
-	};
-  };
+    'no-moment': {
+      meta: {
+        type: "problem",
+        docs: {
+          description: "Don't allow the moment library",
+        },
+        fixable: "code",
+        schema: [],
+      },
+      create(context) {
+        return {
+          ImportDeclaration(node) {
+            if (node.source.value === "moment") {
+              context.report({
+                node,
+                message: "Do not use the library 'moment'",
+              });
+            }
+          },
+        };
+      },
+    },
+  },
 };
